48d039b9552841371679e6704af1f5e6a4103e90
Jeremy Chen <convexset@gmail.com>
2015-11-24 23:07:26 +0800

    Dependency update (to deal with minification problem)

--- packages/progressive-file-hasher/package.js
+++ packages/progressive-file-hasher/package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	name: 'convexset:progressive-file-hasher',
-	version: '0.1.0',
+	version: '0.1.1',
 	summary: 'A client-side file hasher that hashes progressively to reduce impact on the UI',
 	git: 'https://github.com/convexset/meteor-progressive-file-hasher',
 	documentation: '../../README.md'
@@ -11,7 +11,7 @@ Package.onUse(function(api) {
 	api.use([
 		'ecmascript',
 		'underscore',
-		'convexset:package-utils@0.1.0',
+		'convexset:package-utils@0.1.3',
 		'jparker:crypto-core@0.1.0',
 		'jparker:crypto-md5@0.1.1',
 		'jparker:crypto-sha1@0.1.0',
